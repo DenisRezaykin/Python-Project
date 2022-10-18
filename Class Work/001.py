3ea2b56772fe11a4d908ea48373c59177b0158f3
Denis Rezaykin <drezay@mail.ru>
2022-10-18 23:02:12 +0300

    Task 005 v.1.0

--- Class Work/001.py
+++ Class Work/001.py
@@ -25,18 +25,19 @@
 # Задайте список, состоящий из произвольных чисел, количество задает пользователь.
 # Напишите программу, определяющую присутствует ли в заданном списке число, полученное от пользователя.
 
-import random
+# import random
+#
+#
+# def magic(count, find_num2):
+#     if count < 0:
+#         return "Ошибка"
+#     ls = random.sample(range(count * 2), count)
+#     print(ls)
+#     if find_num2 in ls:
+#         return "Yes"
+#     return "No"
+#
+# num1 = int(input('Введите число: '))
+# num2 = int(input('Введите число: '))
+# print(magic(num1, num2))
 
-
-def magic(count, find_num2):
-    if count < 0:
-        return "Ошибка"
-    ls = random.sample(range(count * 2), count)
-    print(ls)
-    if find_num2 in ls:
-        return "Yes"
-    return "No"
-
-num1 = int(input('Введите число: '))
-num2 = int(input('Введите число: '))
-print(magic(num1, num2))
